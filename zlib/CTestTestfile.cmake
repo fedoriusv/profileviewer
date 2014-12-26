2b0982c44390ea316b701bf45282cde74d8a4b4c
fedoriusv <fedoriusv@gmail.com>
2014-12-26 19:27:44 +0200

    +Add zlib

---
+++ zlib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/Git/profileviewer/profileviewer/zlib
+# Build directory: D:/Git/profileviewer/profileviewer/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(example "example")
